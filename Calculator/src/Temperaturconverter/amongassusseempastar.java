641447f3c81d3406d5354ea050cb0d97d3591e0e
Fabian Brenner <fabian@familie-brenner.com>
2024-11-14 16:57:50 +0100

    SIGMA NEW ADDITION???

---
+++ Calculator/src/Temperaturconverter/amongassusseempastar.java
@@ -0,0 +1,18 @@
+package Temperaturconverter;
+
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class amongassusseempastar {
+    public amongassusseempastar() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("WELKUM TO DA SKIBIDI TMPRATUZR CELCULATRO!!!! \n" + "Which shyt yu like do1n??? \n" + "1: Celsus to kevni!! \n" + "0:exit (VERY BAD STEI WIT ME FREVEAH!!!!)");
+        String Aktion;
+        Scanner sc = new Scanner(System.in);
+        Aktion = sc.nextLine();
+
+    }
+}
---
+++ Calculator/src/calculetor/README.md
@@ -0,0 +1,3 @@
+## Simple, Text based calculator, that delivers you an anime villain ahh monologue when trying to divide by 0. 
+#
+No, I wont optimize the monologue. Cry about it 😔
